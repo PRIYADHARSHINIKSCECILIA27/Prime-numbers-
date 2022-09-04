fe535f5ad94e924db291988040ea880c40608500
PRIYADHARSHINIKSCECILIA27 <112814691+PRIYADHARSHINIKSCECILIA27@users.noreply.github.com>
2022-09-04 20:15:53 +0530

    Create Primeno.py

---
+++ Primeno.py
@@ -0,0 +1,13 @@
+for i in range (2,200):
+
+    for j in range (2,i+1):
+
+        if i%j==0 :
+
+            if i==j:
+
+                print("\n%i"%i)
+
+            else:
+
+                break
